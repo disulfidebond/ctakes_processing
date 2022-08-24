cbdc1d0b4a1e887089e374ca6a3395294f0bf499
JOHN R CASKEY <jrcaskey@medicine.wisc.edu>
2022-08-24 10:04:38 -0500

    Update setup_for_flatfile_generation.py

--- code/setup_for_flatfile_generation.py
+++ code/setup_for_flatfile_generation.py
@@ -11,12 +11,11 @@ ts_string = t.strftime('%m%d%Y_%H%M%S')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dirName', '-d', help='name of directory with compressed XMI files', required=True)
-parser.add_argument('--workDirName', '-w', help='name of directory that will hold runfiles')
 parser.add_argument('--dirCt', '-c', help='count for number of files in working directory', nargs='?', const=10000, type=int, default=10000)
 parser.add_argument('--dirLimit', '-l', help='number of simultaneous flatfile instances to allow', nargs='?', const=3, type=int, default=3)
+parser.add_argument('--runLabel', '-r', type=str, help='label for run files')
 args = parser.parse_args()
-print(args.dirCt)
-print(args.dirLimit)
+
 
 # functions
 def writeToOutput(l, setCt, runDirName):
@@ -45,15 +44,21 @@ if checkGZ:
   sys.exit()
 
 # check to make sure dirLimit is 1, 2, or 3
-if int(args.dirLimit) > 0 and int(args.dirLimit) < 4:
+if int(args.dirLimit) > 0 and int(args.dirLimit) <= 20:
   print('ready to run flatfile generation with ' + str(args.dirLimit) + ' simultaneous instances')
 else:
-  print('Error, the number of simultaneous flatfile instances can only be >= 1 or <= 3')
+  print('Error, the number of simultaneous flatfile instances can only be >= 1 or <= 20')
   sys.exit()
 
 # setup
 fNamePath = [(str(args.dirName) + '/' + str(x)) for x in fNameList]
+runFileLabel = 'xmiList.' + ts_string + '.txt'
 runDir = 'fileListDir_' + ts_string
+if args.runLabel:
+  runFileLabel = 'xmiList.' + str(args.runLabel) + '.' + ts_string
+  runDir = str(args.runLabel) + '_' + ts_string
+
+
 Path(runDir).mkdir(parents=True, exist_ok=True)
 
 # create manifest files in runDir
@@ -85,11 +90,11 @@ filesInRunDir = fileCountInRunDir[0]
 fileCountInRunDir = fileCountInRunDir[1]
 filesInRunDir = [((runDir) + '/' + str(x)) for x in filesInRunDir]
 if int(args.dirLimit) == 1:
-  runFileName = 'xmiList.' + ts_string + '.txt'
+  runFileName = runFileLabel + '.txt'
   with open(runFileName, 'w') as fWrite:
     for i in filesInRunDir:
       fWrite.write(str(i) + '\n')
-  print('created runfile for flatfile generator named ' + str(runFileName))
+  print('created runfile for flatfile generator with prefix ' + str(runFileLabel))
 elif int(args.dirLimit) == 2:
   limitRange = int(int(fileCountInRunDir)/int(args.dirLimit))
   skipWrite = False
@@ -102,7 +107,7 @@ elif int(args.dirLimit) == 2:
       if skipWrite:
         writeList.append(filesInRunDir[i])
       else:
-        runFileName = 'xmiList.' + ts_string + '.' + str(ct) + '.txt'
+        runFileName = runFileLabel + '.' + str(ct) + '.txt'
         with open(runFileName, 'w') as fWrite:
           for w in writeList:
             fWrite.write(str(w) + '\n')
@@ -113,18 +118,18 @@ elif int(args.dirLimit) == 2:
     else:
       writeList.append(filesInRunDir[i])
   if skipWrite:
-    runFileName = 'xmiList.' + ts_string + '.' + str(ct) + '.txt'
+    runFileName = runFileLabel + '.' + str(ct) + '.txt'
     with open(runFileName, 'w') as fWrite:
       for w in writeList:
         fWrite.write(str(w) + '\n')
-  print('created runfiles for flatfile generator named xmiList.' + str(ts_string) + '.*.txt')
+  print('created runfiles for flatfile generator with prefix ' + str(runFileLabel))
 else:
   limitRange = int(int(fileCountInRunDir)/int(args.dirLimit))
   ct = 1
   writeList = []
   for i in range(0, fileCountInRunDir):
     if i == limitRange:
-      runFileName = 'xmiList.' + ts_string + '.' + str(ct) + '.txt'
+      runFileName = runFileLabel + '.' + str(ct) + '.txt'
       with open(runFileName, 'w') as fWrite:
         for w in writeList:
           fWrite.write(str(w) + '\n')
@@ -134,9 +139,9 @@ else:
       writeList.append(filesInRunDir[i])
     else:
       writeList.append(filesInRunDir[i])
-    runFileName = 'xmiList.' + ts_string + '.' + str(ct) + '.txt'
+    runFileName = runFileLabel + '.' + str(ct) + '.txt'
   with open(runFileName, 'w') as fWrite:
     for w in writeList:
       fWrite.write(str(w) + '\n')
-  print('created runfiles for flatfile generator named xmiList.' + str(ts_string) + '.*.txt')
+  print('created runfiles for flatfile generator with prefix ' + str(runFileLabel))
 print('Setup of work directories completed')
