82683931d8d5bafb3af4948f200d42927ac312cb
JOHN R CASKEY <jrcaskey@medicine.wisc.edu>
2023-04-28 12:17:09 -0500

    Update runCTAKES.sh

--- code/runCTAKES.sh
+++ code/runCTAKES.sh
@@ -1,9 +1,4 @@
-RUNDIR=$1
-
-if [[ -z $RUNDIR ]] ; then
-  echo 'ERROR, no input dir provided'
-  exit 1
-fi
 TSTRING=$(date +"%d%m%y_%H%M%S")
 
-ctakes_4.0.0.1_runInstance_${RUNDIR}/bin/runPiperFile.mod.sh -p resources/org/apache/ctakes/clinical/pipeline/DefaultFastPipeline.updatedComboDict.piper -l resources/org/apache/ctakes/dictionary/lookup/fast/updatedcombodict.xml -i inputDir --xmiOut outputDir &>> ctakes_${TSTRING}_log.txt
+echo "$PWD" &>> ctakes_${TSTRING}_log.txt
+./bin/runPiperFile.sh -p resources/org/apache/ctakes/clinical/pipeline/DefaultFastPipeline.piper -l resources/org/apache/ctakes/dictionary/lookup/fast/snomedonly.xml -i inputDir --xmiOut outputDir &>> ctakes_${TSTRING}_log.txt
